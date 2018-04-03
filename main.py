893d6f58d91a77ffc5b1d16c90c1b500316f39d6
simon <466718611@qq.com>
2018-04-03 09:20:05 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
 
 """
-distribution job for GPU and run,
+distribution job for GPU and run with tensorflow.
+date: 2018/4/2
+author: simon
 
 """
 
@@ -14,7 +16,7 @@ import os
 
 from conf import *
 
-_ , GPUNUM = get_available_gpus()
+
 
 def get_available_gpus():
 
@@ -23,24 +25,13 @@ def get_available_gpus():
 	GPUlist = [x.name for x in local_device_protos if x.device_type == 'GPU']
 	return GPUlist,len(GPUlist)
 
-
-def worker3():
-	savedStdout = sys.stdout
-	file = open('worker3.out','w')
-	sys.stdout = file
-	job_list = split_worker()[2]
-	os.environ["CUDA_VISIBLE_DEVICES"] = '2'
-	if job_list:
-		for job in job_list:
-			job()
-	sys.stdout = savedStdout
-	file.close()
+_ , GPUNUM = get_available_gpus()
 
 def get_worker(gpu=0):
 
 	savedStdout = sys.stdout
 	job_list,arg_list,name_list = split_worker()
-	job_list,arg_list,name_list = job_list[gpu],arg_list[gpu],namelist[gpu]
+	job_list,arg_list,name_list = job_list[gpu],arg_list[gpu],name_list[gpu]
 	os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
 	if job_list:
 		for i,job in enumerate(job_list):
@@ -48,14 +39,14 @@ def get_worker(gpu=0):
 			sys.stdout = file
 			job(arg_list[i])
 			file.close()
-	print('worker'+str(gpu))
+	#print('worker'+str(gpu))
 	sys.stdout = savedStdout
 
 def split_worker(gpunums=GPUNUM):
 	muti = get_proceseelist()
 	job_list = muti.processlist
 	arg_list = muti.arglist
-	name_list = muti.namelist
+	name_list = muti.filenamelist
 	#print(job_list)
 	rj,ra,rn = [[] for _ in range(gpunums)],[[] for _ in range(gpunums)],[[] for _ in range(gpunums)]
 	for i,job in enumerate(job_list):
@@ -67,9 +58,9 @@ def split_worker(gpunums=GPUNUM):
 if __name__ == '__main__':
 	# process_list = [Process(target=worker1,args=()),Process(target=worker2,args=()),\
 	# Process(target=worker3,args=()),Process(target=worker4,args=())]
-	for p in process_list:
+	process_list = []
 	for i in range(GPUNUM):
-		process_list.append(Process(target=get_worker,args=(i)))
+		process_list.append(Process(target=get_worker,args=(i,)))
 	for s_p in process_list:
 		s_p.start()
 	for s_p in process_list:
